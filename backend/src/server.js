79a3afa731aa23dcb7c7ee8192e8cf20a76f939d
JimFrankS <37932960+JimFrankS@users.noreply.github.com>
2025-07-29 21:58:57 +0200

    startServer Function added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,8 +4,18 @@ import { connectDB } from "./config/db.js"; // Import database connection functi
 
 const app = express();
 
-connectDB(); // Connect to the MongoDB database
-
 app.get("/", (req, res) => res.send("Hello from server")) // Simple route to test server
 
-app.listen(ENV.PORT, () => console.log("Server is running on port", ENV.PORT)); // Start the server on the specified port
+const startServer = async () => {
+    try {
+        await connectDB(); // Connect to the database
+
+        app.listen(ENV.PORT, () => console.log("Server is running on port", ENV.PORT)); // Start the server on the specified port
+
+    } catch (error) {
+        console.error("Error starting server:", error); // Log any errors that occur during server startup
+        process.exit(1); // Exit the process with a failure code
+    }
+};
+
+startServer(); // Call the function to start the server
